7baa9830c6968a30c7166b0dcf9ddc3416a01d7e
Flipster77 <Flipster77@users.noreply.github.com>
2016-06-06 19:00:32 +0930

    Added three more ActionMaster tests, updated ActionMaster to pass them.
    Added T02 to T04 to ActionMasterTest.cs
    Updated ActionMaster to pass them.

--- Assets/Editor/ActionMasterTest.cs
+++ Assets/Editor/ActionMasterTest.cs
@@ -7,7 +7,13 @@ using UnityEngine;
 public class ActionMasterTest {
 
     private ActionMaster.Action endTurn = ActionMaster.Action.EndTurn;
-    private ActionMaster actionMaster = new ActionMaster();
+    private ActionMaster.Action tidy = ActionMaster.Action.Tidy;
+    private ActionMaster actionMaster;
+
+    [SetUp]
+    public void Setup() {
+        actionMaster = new ActionMaster();
+    }
 
     [Test]
     public void T00PassingTest() {
@@ -17,5 +23,22 @@ public class ActionMasterTest {
     [Test]
     public void T01OneStrikeReturnsEndTurn() {
         Assert.AreEqual(endTurn, actionMaster.RecordBowl(10));
+    }
+
+    [Test]
+    public void T02BowlEightReturnsTidy() {
+        Assert.AreEqual(tidy, actionMaster.RecordBowl(8));
+    }
+
+    [Test]
+    public void T03BowlTwoThenEightReturnsEndTurn() {
+        Assert.AreEqual(tidy, actionMaster.RecordBowl(8));
+        Assert.AreEqual(endTurn, actionMaster.RecordBowl(2));
+    }
+
+    [Test]
+    public void T04TwoGutterBalls() {
+        Assert.AreEqual(tidy, actionMaster.RecordBowl(0));
+        Assert.AreEqual(endTurn, actionMaster.RecordBowl(0));
     }
 }
--- Assets/Scripts/ActionMaster.cs
+++ Assets/Scripts/ActionMaster.cs
@@ -5,15 +5,26 @@ public class ActionMaster {
 
     public enum Action {Tidy, Reset, EndTurn, EndGame};
 
+    //private int[] bowls = new int[21];
+    private int bowl = 1;
+
 	public Action RecordBowl(int pins) {
         if (pins < 0 || pins > 10) {
             throw new UnityException("Pins bowled must be between 0 and 10");
         }
 
         if (pins == 10) {
+            bowl += 2;
             return Action.EndTurn;
         }
 
-        throw new UnityException("What you talking 'bout Fry?");
+        // Mid frame, return tidy
+        if (bowl % 2 != 0) {
+            bowl++;
+            return Action.Tidy;
+        } else { // End of frame
+            bowl++;
+            return Action.EndTurn;
+        }
     }
 }
