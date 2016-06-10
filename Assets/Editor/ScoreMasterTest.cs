af463c4a4c7c82bf174b3b16bcd28f9b54374965
Flipster77 <Flipster77@users.noreply.github.com>
2016-06-10 19:54:16 +0930

    Added 23 ScoreMaster tests and initial implementation of ScoreMaster.
    ScoreMaster returns a list of scores for each frame, rather than the
    running score in each frame. This will be refactored later. Currently
    passing all 23 tests.

---
+++ Assets/Editor/ScoreMasterTest.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+
+[TestFixture]
+public class ScoreMasterTest {
+
+    [SetUp]
+    public void Setup() {
+        
+    }
+
+    [Test]
+    public void T00PassingTest() {
+        Assert.AreEqual(1, 1);
+    }
+
+    [Test]
+    public void T01ScoreASingleFrame() {
+        List<int> bowls = new List<int>(new int[] { 7, 1 });
+        List<int> scores = ScoreMaster.ScoreFrames(bowls);
+
+        Assert.AreEqual(8, scores[0]);
+    }
+
+    [Test]
+    public void T02ScoreThreeBowls() {
+        List<int> bowls = new List<int>(new int[] { 7, 1, 5 });
+        List<int> scores = ScoreMaster.ScoreFrames(bowls);
+
+        Assert.AreEqual(8, scores[0]);
+        Assert.AreEqual(ScoreMaster.SCORE_UNKNOWN, scores[1]);
+    }
+
+    [Test]
+    public void T03ScoreOneSpare() {
+        List<int> bowls = new List<int>(new int[] { 7, 3 });
+        List<int> scores = ScoreMaster.ScoreFrames(bowls);
+
+        Assert.AreEqual(ScoreMaster.SCORE_UNKNOWN, scores[0]);
+    }
+
+    [Test]
+    public void T04ScoreOneStrike() {
+        List<int> bowls = new List<int>(new int[] { 10, 0 });
+        List<int> scores = ScoreMaster.ScoreFrames(bowls);
+
+        Assert.AreEqual(ScoreMaster.SCORE_UNKNOWN, scores[0]);
+    }
+
+    [Test]
+    public void T05ScoreASpareAfterOneFollowUpBowl() {
+        List<int> bowls = new List<int>(new int[] { 7, 3, 5 });
+        List<int> scores = ScoreMaster.ScoreFrames(bowls);
+
+        Assert.AreEqual(15, scores[0]);
+        Assert.AreEqual(ScoreMaster.SCORE_UNKNOWN, scores[1]);
+    }
+
+    [Test]
+    public void T06ScoreASpareFollowedByGutterball() {
+        List<int> bowls = new List<int>(new int[] { 6, 4, 0 });
+        List<int> scores = ScoreMaster.ScoreFrames(bowls);
+
+        Assert.AreEqual(10, scores[0]);
+        Assert.AreEqual(ScoreMaster.SCORE_UNKNOWN, scores[1]);
+    }
+
+    [Test]
+    public void T07ScoreAStrikeAfterOneFollowUpBowl() {
+        List<int> bowls = new List<int>(new int[] { 10, 0, 5 });
+        List<int> scores = ScoreMaster.ScoreFrames(bowls);
+
+        Assert.AreEqual(ScoreMaster.SCORE_UNKNOWN, scores[0]);
+        Assert.AreEqual(ScoreMaster.SCORE_UNKNOWN, scores[1]);
+    }
+
+    [Test]
+    public void T08ScoreAStrikeAfterTwoFollowUpBowls() {
+        List<int> bowls = new List<int>(new int[] { 10, 0, 5, 3 });
+        List<int> scores = ScoreMaster.ScoreFrames(bowls);
+
+        Assert.AreEqual(18, scores[0]);
+        Assert.AreEqual(8, scores[1]);
+    }
+
+    [Test]
+    public void T09ScoreASpareFollowedByOneGutterball() {
+        List<int> bowls = new List<int>(new int[] {10, 0, 0, 6 });
+        List<int> scores = ScoreMaster.ScoreFrames(bowls);
+
+        Assert.AreEqual(16, scores[0]);
+        Assert.AreEqual(6, scores[1]);
+    }
+
+    [Test]
+    public void T10ScoreASpareFollowedByTwoGutterballs() {
+        List<int> bowls = new List<int>(new int[] { 10, 0, 0, 0 });
+        List<int> scores = ScoreMaster.ScoreFrames(bowls);
+
+        Assert.AreEqual(10, scores[0]);
+        Assert.AreEqual(0, scores[1]);
+    }
+
+    [Test]
+    public void T11ScoreTwoSpares() {
+        List<int> bowls = new List<int>(new int[] { 7, 3, 8, 2 });
+        List<int> scores = ScoreMaster.ScoreFrames(bowls);
+
+        Assert.AreEqual(18, scores[0]);
+        Assert.AreEqual(ScoreMaster.SCORE_UNKNOWN, scores[1]);
+    }
+
+    [Test]
+    public void T12ScoreTwoStrikes() {
+        List<int> bowls = new List<int>(new int[] { 10, 0, 10, 0 });
+        List<int> scores = ScoreMaster.ScoreFrames(bowls);
+
+        Assert.AreEqual(ScoreMaster.SCORE_UNKNOWN, scores[0]);
+        Assert.AreEqual(ScoreMaster.SCORE_UNKNOWN, scores[1]);
+    }
+
+    [Test]
+    public void T13ScoreThreeStrikes() {
+        List<int> bowls = new List<int>(new int[] { 10, 0, 10, 0, 10, 0 });
+        List<int> scores = ScoreMaster.ScoreFrames(bowls);
+
+        Assert.AreEqual(30, scores[0]);
+        Assert.AreEqual(ScoreMaster.SCORE_UNKNOWN, scores[1]);
+        Assert.AreEqual(ScoreMaster.SCORE_UNKNOWN, scores[2]);
+    }
+
+    [Test]
+    public void T14ScoreFourStrikes() {
+        List<int> bowls = new List<int>(new int[] { 10, 0, 10, 0, 10, 0, 10, 0 });
+        List<int> scores = ScoreMaster.ScoreFrames(bowls);
+
+        Assert.AreEqual(30, scores[0]);
+        Assert.AreEqual(30, scores[1]);
+        Assert.AreEqual(ScoreMaster.SCORE_UNKNOWN, scores[2]);
+        Assert.AreEqual(ScoreMaster.SCORE_UNKNOWN, scores[3]);
+    }
+
+    [Test]
+    public void T15ScoreFourBowls() {
+        List<int> bowls = new List<int>(new int[] { 6, 3, 7, 1 });
+        List<int> scores = ScoreMaster.ScoreFrames(bowls);
+
+        Assert.AreEqual(9, scores[0]);
+        Assert.AreEqual(8, scores[1]);
+    }
+
+    [Test]
+    public void T16ScoreOneWholeGame() {
+        List<int> bowls = new List<int>(new int[] { 6, 3, 7, 1, 5, 5, 8, 1, 9, 0, 2, 4, 10, 0, 7, 2, 5, 2, 0, 8 });
+        List<int> expectedScores = new List<int>(new int[] { 9, 8, 18, 9, 9, 6, 19, 9, 7, 8 });
+
+        List<int> scores = ScoreMaster.ScoreFrames(bowls);
+
+        for (int i = 0; i < scores.Count; i++) {
+            Assert.AreEqual(expectedScores[i], scores[i]);
+        }
+    }
+
+    [Test]
+    public void T17ScoreGameWithSpareInFrame10() {
+        List<int> bowls = new List<int>(new int[] { 6, 3, 7, 1, 5, 5, 8, 1, 9, 0, 2, 4, 10, 0, 7, 2, 5, 2, 3, 7, 6 });
+        List<int> expectedScores = new List<int>(new int[] { 9, 8, 18, 9, 9, 6, 19, 9, 7, 16 });
+
+        List<int> scores = ScoreMaster.ScoreFrames(bowls);
+
+        for (int i = 0; i < scores.Count; i++) {
+            Assert.AreEqual(expectedScores[i], scores[i]);
+        }
+    }
+
+    [Test]
+    public void T18ScoreGameWithZeroTenSpareInFrame10() {
+        List<int> bowls = new List<int>(new int[] { 6, 3, 7, 1, 5, 5, 8, 1, 9, 0, 2, 4, 10, 0, 7, 2, 5, 2, 0, 10, 5 });
+        List<int> expectedScores = new List<int>(new int[] { 9, 8, 18, 9, 9, 6, 19, 9, 7, 15 });
+
+        List<int> scores = ScoreMaster.ScoreFrames(bowls);
+
+        for (int i = 0; i < scores.Count; i++) {
+            Assert.AreEqual(expectedScores[i], scores[i]);
+        }
+    }
+
+    [Test]
+    public void T19ScoreGameWithOneStrikeInFrame10() {
+        List<int> bowls = new List<int>(new int[] { 6, 3, 7, 1, 5, 5, 8, 1, 9, 0, 2, 4, 10, 0, 7, 2, 5, 2, 10, 6, 3 });
+        List<int> expectedScores = new List<int>(new int[] { 9, 8, 18, 9, 9, 6, 19, 9, 7, 19 });
+
+        List<int> scores = ScoreMaster.ScoreFrames(bowls);
+
+        for (int i = 0; i < scores.Count; i++) {
+            Assert.AreEqual(expectedScores[i], scores[i]);
+        }
+    }
+
+    [Test]
+    public void T20ScoreGameWithTwoStrikesInFrame10() {
+        List<int> bowls = new List<int>(new int[] { 6, 3, 7, 1, 5, 5, 8, 1, 9, 0, 2, 4, 10, 0, 7, 2, 5, 2, 10, 10, 5 });
+        List<int> expectedScores = new List<int>(new int[] { 9, 8, 18, 9, 9, 6, 19, 9, 7, 25 });
+
+        List<int> scores = ScoreMaster.ScoreFrames(bowls);
+
+        for (int i = 0; i < scores.Count; i++) {
+            Assert.AreEqual(expectedScores[i], scores[i]);
+        }
+    }
+
+    [Test]
+    public void T21ScoreGameOfAllGutterballs() {
+        List<int> bowls = new List<int>(new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 });
+        List<int> expectedScores = new List<int>(new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 });
+
+        List<int> scores = ScoreMaster.ScoreFrames(bowls);
+
+        for (int i = 0; i < scores.Count; i++) {
+            Assert.AreEqual(expectedScores[i], scores[i]);
+        }
+    }
+
+    [Test]
+    public void T22ScoreGameOfAllSpares() {
+        List<int> bowls = new List<int>(new int[] { 0, 10, 1, 9, 2, 8, 3, 7, 4, 6, 5, 5, 6, 4, 7, 3, 8, 2, 9, 1, 10 });
+        List<int> expectedScores = new List<int>(new int[] { 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 });
+
+        List<int> scores = ScoreMaster.ScoreFrames(bowls);
+
+        for (int i = 0; i < scores.Count; i++) {
+            Assert.AreEqual(expectedScores[i], scores[i]);
+        }
+    }
+
+    [Test]
+    public void T23PerfectGame() {
+        List<int> bowls = new List<int>(new int[] { 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 10, 10 });
+        List<int> expectedScores = new List<int>(new int[] { 30, 30, 30, 30, 30, 30, 30, 30, 30, 30 });
+
+        List<int> scores = ScoreMaster.ScoreFrames(bowls);
+
+        for (int i = 0; i < scores.Count; i++) {
+            Assert.AreEqual(expectedScores[i], scores[i]);
+        }
+    }
+}
--- Assets/Scripts/ScoreMaster.cs
+++ Assets/Scripts/ScoreMaster.cs
@@ -4,11 +4,95 @@ using System.Collections.Generic;
 
 public class ScoreMaster {
 
+    public const int SCORE_UNKNOWN = -1;
+
 	public static List<int> ScoreFrames (List<int> bowls) {
         List<int> frameList = new List<int>();
 
-        //TODO return list of frame scores
+        int bowlNum = 0;
+        for (int i = 0; i < bowls.Count / 2; i++) {
+            // Frame score is usually first bowl + second bowl
+            int frameScore = bowls[bowlNum] + bowls[bowlNum + 1];
+
+            // Strike occurred
+            if (bowls[bowlNum] == 10) {
+                frameScore = GetStrikeScore(bowls, bowlNum);
+            }
+            // Spare occurred
+            else if (bowls[bowlNum] + bowls[bowlNum + 1] == 10) {
+                frameScore = GetSpareScore(bowls, bowlNum);
+            }
+
+            frameList.Add(frameScore);
+            bowlNum += 2;
+        }
+
+        // If the current frame isn't finished, then the frame score is unknown
+        // Ignore this for the 21st bowl, as the score is handled by the spare/strike
+        if (bowls.Count % 2 != 0 && bowls.Count <= 20) {
+            frameList.Add(SCORE_UNKNOWN);
+        }
+
+        string debugLog = "Frame list:";
+        foreach (int i in frameList) {
+            debugLog += " " + i;
+        }
+        Debug.Log(debugLog);
 
         return frameList;
+    }
+
+    private static int GetStrikeScore(List<int> bowls, int bowlNum) {
+        int strikeScore = 10;
+
+        int nextBowl = bowlNum + 2; //Skip the zero after the 10
+
+        if (bowlNum >= 18) { // In the last frame, only increment bowlNum by 1
+            nextBowl = bowlNum + 1;
+        }
+
+        // Next bowl has not occurred yet, score is unknown
+        if (bowls.Count < nextBowl+1) {
+            return SCORE_UNKNOWN;
+        }
+        // Otherwise add next bowl to the strike score
+        else {
+            strikeScore += bowls[nextBowl];
+        }
+
+        // If the next bowl was also a strike, skip another zero
+        if (bowls[nextBowl] == 10 && bowlNum < 18) {
+            nextBowl += 2;
+        } else {
+            nextBowl++;
+        }
+
+        // Second bowl after the strike has not occurred yet, score is unknown
+        if (bowls.Count < nextBowl+1) {
+            return SCORE_UNKNOWN;
+        }
+        // Otherwise add second bowl to the strike score
+        else {
+            strikeScore += bowls[nextBowl];
+        }
+
+        return strikeScore;
+    }
+
+    private static int GetSpareScore(List<int> bowls, int bowlNum) {
+        int spareScore = 10;
+
+        int nextBowl = bowlNum + 2; // Get the bowl in the next frame
+
+        // Next bowl has not occurred yet, score is unknown
+        if (bowls.Count < nextBowl + 1) {
+            return SCORE_UNKNOWN;
+        }
+        // Otherwise add next bowl to the strike score
+        else {
+            spareScore += bowls[nextBowl];
+        }
+
+        return spareScore;
     }
 }
