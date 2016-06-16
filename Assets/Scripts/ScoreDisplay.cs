e2607613a9115246b02be83ea3a65ba0454a512d
Flipster77 <Flipster77@users.noreply.github.com>
2016-06-16 12:50:08 +0930

    Added score display to UI, currently non functional but looks good.

---
+++ Assets/Scripts/ScoreDisplay.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ScoreDisplay : MonoBehaviour {
+
+    public Text[] bowls;
+    public Text[] frameScores;
+
+	// Use this for initialization
+	void Start () {
+        SetBowlNumbers();
+        SetScoreNumbers();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    private void SetBowlNumbers() {
+        int i = 1;
+        foreach (Text bowlDisplay in bowls) {
+            bowlDisplay.text = i.ToString();
+            i++;
+        }
+    }
+
+    private void SetScoreNumbers() {
+        int i = 1;
+        foreach (Text scoreDisplay in frameScores) {
+            scoreDisplay.text = i.ToString();
+            i++;
+        }
+    }
+}
