311cdfdafa335599bc6a6c660b930858abdc309b
Flipster77 <Flipster77@users.noreply.github.com>
2016-09-05 21:13:36 +0930

    Added skybox and sun rotation to game scene.

---
+++ Assets/Utilities/SkyboxRotater.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkyboxRotater : MonoBehaviour {
+
+    public float rotationSpeed;
+    private float rotation;
+
+    private Light directionalLight;
+
+    void Start() {
+        directionalLight = FindObjectOfType<Light>();
+    }
+
+	
+	void Update () {
+        rotation += rotationSpeed * Time.deltaTime;
+        rotation %= 360f;
+        Material skyboxMaterial = new Material(RenderSettings.skybox);
+        skyboxMaterial.SetFloat("_Rotation", rotation);
+        RenderSettings.skybox = skyboxMaterial;
+
+        directionalLight.transform.Rotate(0f, -rotationSpeed * Time.deltaTime, 0f, Space.World);
+    }
+}
