1385fe8cc9cce9a5f6094c3486e3e4e12e62aba3
Flipster77 <Flipster77@users.noreply.github.com>
2016-09-27 20:23:47 +0930

    Added Platform dependent content switching.
    Added standalone descriptions to options and howtoplay, added
    PlatformContentSwapper to change the content depending on the build.
    Added icon to Player settings.

---
+++ Assets/Utilities/PlatformContentSwapper.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformContentSwapper : MonoBehaviour {
+
+    /// <summary>
+    /// Mobile platform only content in the scene.
+    /// </summary>
+    public GameObject[] mobileContent;
+    /// <summary>
+    /// Standalone platform only content in the scene.
+    /// </summary>
+    public GameObject[] standaloneContent;
+
+	// Use this for initialization
+	void Awake () {
+        RemoveContentForOtherPlatforms();
+	}
+	
+    /// <summary>
+    /// Removes the content for platforms other than the current platform.
+    /// </summary>
+    private void RemoveContentForOtherPlatforms() {
+        #if UNITY_STANDALONE
+            RemoveMobileContent();
+        #else
+            RemoveStandaloneContent();
+        #endif
+    }
+
+    /// <summary>
+    /// Removes the mobile platform only content from the scene.
+    /// </summary>
+	private void RemoveMobileContent() {
+        foreach (GameObject content in mobileContent) {
+            GameObject.Destroy(content);
+        }
+    }
+
+    /// <summary>
+    /// Removes the standalone platform only content from the scene.
+    /// </summary>
+    private void RemoveStandaloneContent() {
+        foreach (GameObject content in standaloneContent) {
+            GameObject.Destroy(content);
+        }
+    }
+}
